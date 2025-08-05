6796a884f70d95ab7667ecc08614dc889374e8bf
Max Kroft <kroftmaxwell@gmail.com>
2025-08-05 15:01:54 -0500

    added duration and num_periods

--- Orbeez.py
+++ Orbeez.py
@@ -6,11 +6,14 @@ import os
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
 from astropy import units as u
 
-def make_orbit_gif(a_list, p_list, r_list, directory, name, figsize=(8,8), color_list=None, increments=100, title = False):
+def make_orbit_gif(a_list, p_list, r_list, directory, name, figsize=(8,8), num_periods = 1, gif_duration = 10, color_list=None, increments=100, title = False):
     if color_list is None:
         color_list = [None] * len(a_list)
 
     p_list = np.array(p_list)/max(p_list)
+
+    if np.min(p_list)*increments < 16:
+        increments = int(np.ceil(16/np.min(p_list)))
 
     planet_list = []
 
@@ -20,19 +23,19 @@ def make_orbit_gif(a_list, p_list, r_list, directory, name, figsize=(8,8), color
 
     for j in range(increments):
         for planet in planet_list:
-            planet.update_pos(j/increments)
+            planet.update_pos(j/increments*num_periods)
         plot_orbit(planet_list, directory, name, j, figsize, title = title)
 
     frames = [Image.open(directory+'/'+name+'_'+str(i)+'.jpg') for i in range(increments)]
 
     frame_1 = frames[0]
-    frame_1.save(directory+'/'+name+'.gif', format='GIF', append_images=frames, save_all=True, duration=100, loop=0)
+    frame_1.save(directory+'/'+name+'.gif', format='GIF', append_images=frames, save_all=True, duration=gif_duration/increments*1000, loop=0)
 
     for j in range(increments):
         os.remove(directory+'/'+name+'_'+str(j)+'.jpg')
 
 
-def gif_from_archive(system_name: str, directory, name, figsize=(8,8), color_list=None, increments=100, title = False):
+def gif_from_archive(system_name: str, directory, name, figsize=(8,8), num_periods = 1, gif_duration = 10, color_list=None, increments=100, title = False):
     data = NasaExoplanetArchive.query_criteria(
         table="ps", 
         select="pl_name, pl_orbsmax, pl_orbper, pl_radj, st_rad", 
@@ -43,5 +46,5 @@ def gif_from_archive(system_name: str, directory, name, figsize=(8,8), color_lis
     p_list = data['pl_orbper'].value
     r_list = (data['pl_radj'].to(u.Rsun)/data['st_rad']).value
 
-    make_orbit_gif(a_list, p_list, r_list, directory=directory, name=name, figsize=figsize, color_list=color_list, increments=increments, title=title)
+    make_orbit_gif(a_list, p_list, r_list, directory=directory, name=name, figsize=figsize, num_periods=num_periods, gif_duration=gif_duration, color_list=color_list, increments=increments, title=title)
 
