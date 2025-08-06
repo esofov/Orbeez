6b9435955112048295a14d6bba49fdfc23fb30f2
Eli Sofovich <eli.sofovich@gmail.com>
2025-08-06 11:39:16 -0500

    Added docstrings to orbitplot.py

--- Orbeez/orbitplot.py
+++ Orbeez/orbitplot.py
@@ -2,7 +2,20 @@ import matplotlib.pyplot as plt
 
 
 def plot_orbit(planet_list: list, directory: str, name: str, num: int, figsize: tuple, title = False, dpi = 200,star_color='orange'):
+    """Plots planetary system
 
+    Plots the plantary system orbits given a list of planets to be saved as a gif.
+
+    Args:
+        planetary_list (list): List of Planet objects.
+        directory (str): Path to which plots and resulting .gif animation will be saved.
+        name (str): Name of plot files to be generated and the resulting .gif animation.
+        num (str): Number representing order of plot in .gif animation frames.
+        figsize (tuple): matplotlib figure size in (height [in], width [in]). Default is (8, 8)
+        title (bool, optional): Binary determiner for plot title to be included in each plot and the resulting .gif animation. Default is False.
+        dpi (int, optional): DPI for each plot. Default is 200
+        star_color (str, optional): matplotlib color to use for the star. Default is 'orange'.
+    """
     fig, ax = plt.subplots(figsize = figsize, layout = 'constrained')
 
     star = plt.Circle((0,0), 1, color = star_color)
@@ -34,6 +47,16 @@ def plot_orbit(planet_list: list, directory: str, name: str, num: int, figsize: 
     plt.close()
 
 def get_star_color(bp_rp):
+    """Sets color of star for plots
+
+    Defines the color of the star when passed data queried from Exoplanet Archive given BP/RP from Gaia DR2.
+
+    Args:
+        bp_rp (float): Color value pulled from Gaia DR2 with listed Exoplanet Archive Gaia_id using BP/RP.
+
+    Returns:
+        str: hexcode color string
+    """
     colors = [
         {"min": -3, "max": -0.26,  "color": '#a200ff'},
         {"min": -0.25, "max": -0.038, "color": '#0092ed'},
