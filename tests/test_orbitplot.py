426a2efc42eaec0aa18e74d5dfe3de3c4a88f138
Max Kroft <kroftmaxwell@gmail.com>
2025-08-07 15:30:39 -0500

    hopefully fixed test_orbitplot

--- tests/test_orbitplot.py
+++ tests/test_orbitplot.py
@@ -1,13 +1,14 @@
 import os
 from Orbeez import orbitplot
 from Orbeez.planet import Planet
+import numpy as np
 
 
 def test_plot_orbit():
     """
     Tests whether plot_orbit generates a .png file of a single GIF frame
     """
-    p = Planet(1, 1, 1, 'blue')
+    p = Planet(1, 1, 1, 0, np.pi/2, 'blue')
 
     orbitplot.plot_orbit(p, directory='tests/', name='test_plot', num=1, figsize=(8, 8))
 
