fixed nan omega, added planet radius scaling option

Orbeez/Orbeez.py
@@ -10,7 +10,7 @@ from astroquery.gaia import Gaia
 
 
 
-def make_orbit_gif(a_list, p_list, r_list, directory, name, e_list = None, w_list = None, figsize=(8,8), num_periods = 1, gif_duration = 10.0, color_list=None, star_color='orange', num_frames=100, title = False, dpi = 200):
+def make_orbit_gif(a_list, p_list, r_list, directory, name, e_list = None, w_list = None, figsize=(8,8), num_periods = 1, gif_duration = 10.0, color_list=None, star_color='orange', num_frames=100, title = False, dpi = 200, planet_r_scale = 25):
     """Makes a .gif animation of the orbits of the input planetary system.
 
     Args:
@@ -36,6 +36,7 @@ def make_orbit_gif(a_list, p_list, r_list, directory, name, e_list = None, w_lis
         title (bool, optional): Whether or not to include the name as a title above the animation. Default is False.
         dpi (int, optional): Dots per inch to use when saving the frames. If the kernel is crashing, try reducing the
             dpi. Default is 200.
+        planet_r_scale (float, optional): Factor by which to scale up the planet radii. Default is 25.
 
     """
 
@@ -69,7 +70,7 @@ def make_orbit_gif(a_list, p_list, r_list, directory, name, e_list = None, w_lis
     planet_list = []
 
     for i in range(len(a_list)):
-        entry = Planet(a_list[i], p_list[i], r_list[i], e_list[i], w_list[i], color_list[i])
+        entry = Planet(a_list[i], p_list[i], r_list[i], e_list[i], w_list[i], color_list[i], planet_r_scale = planet_r_scale)
         planet_list.append(entry)
 
     print('Generating images...')
@@ -89,7 +90,7 @@ def make_orbit_gif(a_list, p_list, r_list, directory, name, e_list = None, w_lis
         os.remove(directory+'/'+name+'_'+str(j)+'.jpg')
 
 
-def gif_from_archive(system_name, directory, figsize=(8,8), num_periods = 1, gif_duration = 10.0, color_list=None, num_frames=100, title = False, dpi = 200):
+def gif_from_archive(system_name, directory, figsize=(8,8), num_periods = 1, gif_duration = 10.0, color_list=None, num_frames=100, title = False, dpi = 200, planet_r_scale = 25):
      
     """Generates gif of exoplanet system with user entered name from NASA Exoplanet Archive.
 
@@ -108,6 +109,7 @@ def gif_from_archive(system_name, directory, figsize=(8,8), num_periods = 1, gif
         title (bool, optional): Whether or not to include the name as a title above the animation. Default is False.
         dpi (int, optional): Dots per inch to use when saving the frames. If the kernel is crashing, try reducing the
             dpi. Default is 200.
+        planet_r_scale (float, optional): Factor by which to scale up the planet radii. Default is 25.
     """
     
     data = NasaExoplanetArchive.query_criteria(
@@ -123,9 +125,15 @@ def gif_from_archive(system_name, directory, figsize=(8,8), num_periods = 1, gif
     r_list = (data['pl_radj'].to(u.Rsun)/data['st_rad']).value
     e_list = (data['pl_orbeccen']).value
     w_list = (data['pl_orblper']).value * np.pi/180
-    i = np.where(np.isnan(e_list))
+
+    i = np.where(np.isnan(e_list))[0]
     e_list[i] = 0
     w_list[i] = np.pi/2
+
+    if np.any(np.isnan(w_list)):
+        print('Some nan arguments of periastron, setting to pi/2.')
+        i = np.where(np.isnan(w_list))[0]
+        w_list[i] = np.pi/2
 
     gaiaid=data['gaia_id'][0].split()[2]
     query = f"SELECT bp_rp FROM gaiadr2.gaia_source WHERE source_id = {gaiaid}"
@@ -135,5 +143,5 @@ def gif_from_archive(system_name, directory, figsize=(8,8), num_periods = 1, gif
     star_color=get_star_color(bp_rp)
     
 
-    make_orbit_gif(a_list, p_list, r_list, directory=directory, name=system_name, e_list = e_list, w_list = w_list, figsize=figsize, num_periods=num_periods, gif_duration=gif_duration, color_list=color_list, star_color=star_color, num_frames=num_frames, title=title, dpi=dpi)
+    make_orbit_gif(a_list, p_list, r_list, directory=directory, name=system_name, e_list = e_list, w_list = w_list, figsize=figsize, num_periods=num_periods, gif_duration=gif_duration, color_list=color_list, star_color=star_color, num_frames=num_frames, title=title, dpi=dpi, planet_r_scale = planet_r_scale)
 