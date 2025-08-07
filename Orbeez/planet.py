595b0f4e806666e1ea7ca70c0b7fc8ec3c944456
Max Kroft <kroftmaxwell@gmail.com>
2025-08-07 12:02:37 -0500

    added solving for big E

--- Orbeez/planet.py
+++ Orbeez/planet.py
@@ -1,8 +1,8 @@
 import numpy as np
-
+from scipy.optimize import fsolve
 
 class Planet:
-    def __init__(self, a, p, r, color):
+    def __init__(self, a, p, r, e, w, color):
         self.a = a
         self.r = r*25
         if color is None:
@@ -12,8 +12,13 @@ class Planet:
         self.x  = 0
         self.y = self.a
         self.p = p
+        self.e = e
+        self.w = w
 
     def update_pos(self, t):
         theta = t/self.p*2*np.pi + np.pi/2
         self.x = self.a*np.cos(theta)
         self.y = self.a*np.sin(theta)
+
+    def solve_for_E(self, t):
+        return fsolve(lambda x: x - self.e * np.sin(x) - 2*np.pi / self.p * t, 2*np.pi / self.p * t)[0]
