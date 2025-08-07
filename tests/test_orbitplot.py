471b855bba76e2758d03d1acced80fdea545a143
Max Kroft <kroftmaxwell@gmail.com>
2025-08-07 15:32:31 -0500

    trying again with test_orbitplot

--- tests/test_orbitplot.py
+++ tests/test_orbitplot.py
@@ -10,6 +10,6 @@ def test_plot_orbit():
     """
     p = Planet(1, 1, 1, 0, np.pi/2, 'blue')
 
-    orbitplot.plot_orbit(p, directory='tests/', name='test_plot', num=1, figsize=(8, 8))
+    orbitplot.plot_orbit([p], directory='tests/', name='test_plot', num=1, figsize=(8, 8))
 
     assert os.path.isfile('tests/test_plot_1.jpg')
